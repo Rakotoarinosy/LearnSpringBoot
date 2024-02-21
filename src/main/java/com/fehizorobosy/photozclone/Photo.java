80bdfc3c2a4570c76de19e13138f19e8944fe9a8
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-02-21 23:48:01 +0300

    getAll an get unique

---
+++ src/main/java/com/fehizorobosy/photozclone/Photo.java
@@ -0,0 +1,35 @@
+package com.fehizorobosy.photozclone;
+
+public class Photo {
+    
+    private String id;
+
+    private String fileName;
+
+    public Photo() {
+        
+    }
+
+    public Photo(String id,String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    // row data
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
