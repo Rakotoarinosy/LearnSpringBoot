93e9f78c54b0f4eabc7479476de2a4ec668e25a3
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-05-30 15:49:56 +0300

    effacer le dossier patient

--- Patient/firstApp/src/main/java/com/example/first_app/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.first_app.repository;
-
-
-import com.example.first_app.entity.Patient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientRepository extends CrudRepository<Patient,Integer>{
-    
-}
--- Patient/front-end/src/app/Entity/patient.ts
+++
@@ -1,8 +0,0 @@
-export class Patient {
-    id?: number;
-    nom?: string;
-    age?: number;
-    dateDeNaissance?: string;
-    email?: string;
-    genre?: string;
-}
